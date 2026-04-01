06fc6dc3054771e3d32b23ac438c2ffab09df3ff
ygor <ygorsouzasimao@gmail.com>
2026-03-31 21:56:41 -0300

    string

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿﻿string Mensagem, MemeToNervoso;
+Console.Write("Usuário, o que você pensa sobre mim? ");
+Mensagem = Console.ReadLine()!;
+
+MemeToNervoso =
+    $"{Mensagem.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+Console.WriteLine(MemeToNervoso);
